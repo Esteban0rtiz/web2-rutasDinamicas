b8f6f79a31883f27e8470887ea13a4ec726670d0
Esteban0rtiz <116740539+Esteban0rtiz@users.noreply.github.com>
2024-01-30 00:25:53 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,22 +1,23 @@
-uaimport smtplib
+import smtplib
 from email.mime.text import MIMEText
 
 def send_email(subject, body, to_email):
-    # Configura los detalles de la cuenta de correo
-    from_email = "ecuaplush20244@gmail.com"  # Reemplaza con tu dirección de correo electrónico
-    password = "plus2021"          # Reemplaza con tu contraseña
+    from_email = "ecuaplush20244@gmail.com"
+    app_password = "zpbk hakh swel ehyg" 
 
-    # Crea el mensaje de correo
     message = MIMEText(body)
     message["Subject"] = subject
     message["From"] = from_email
     message["To"] = to_email
 
-    # Conéctate al servidor SMTP y envía el correo
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(from_email, password)
-        server.sendmail(from_email, to_email, message.as_string())
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(from_email, app_password)
+            server.sendmail(from_email, to_email, message.as_string())
+        print("Correo enviado con éxito.")
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
 
 # Ejemplo de uso
 send_email("Prueba de Jenkins", "Este es un correo de prueba con script python.", "ecuaplush010@gmail.com")
