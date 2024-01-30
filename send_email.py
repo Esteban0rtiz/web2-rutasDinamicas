387824a3351038a31df519833f93a3ee14cd67d1
Esteban0rtiz <116740539+Esteban0rtiz@users.noreply.github.com>
2024-01-30 00:00:19 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,14 +2,17 @@ import smtplib
 from email.mime.text import MIMEText
 
 def send_email(subject, body, to_email):
-    from_email = "tu_correo@gmail.com"  # Reemplaza con tu dirección de correo electrónico
-    password = "tu_contraseña"          # Reemplaza con tu contraseña
+    # Configura los detalles de la cuenta de correo
+    from_email = "ecuaplush20244@gmail.com"  # Reemplaza con tu dirección de correo electrónico
+    password = "plus2021"          # Reemplaza con tu contraseña
 
+    # Crea el mensaje de correo
     message = MIMEText(body)
     message["Subject"] = subject
     message["From"] = from_email
     message["To"] = to_email
 
+    # Conéctate al servidor SMTP y envía el correo
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login(from_email, password)
